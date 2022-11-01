b184480a267856143664f368bbb57801d8a8b943
AlexRGB <alexismtz200326@gmail.com>
2022-11-01 13:46:05 -0600

    Restauracion de Enviroment

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBTf5v23xPSuDAIFYpJyz4sqLxBUc3C9CQ',
+    authDomain: 'angular-firebase-crud-a3806.firebaseapp.com',
+    projectId: 'angular-firebase-crud-a3806',
+    storageBucket: 'angular-firebase-crud-a3806.appspot.com',
+    messagingSenderId: '381000560215',
+    appId: '1:381000560215:web:c1aa4308c80efc6803fd12',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 import Mapboxgl from 'mapbox-gl';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 
 Mapboxgl.accessToken =
   'pk.eyJ1IjoiYWxleHJnYjIiLCJhIjoiY2w5OHU2a2MwMGdkZTN0bnR1bWFncGtidyJ9.aipf1lD0PglqNtUsHHRpdw';
@@ -10,7 +12,9 @@ if (!navigator.geolocation) {
   alert('Navegador no soporta la Geolocation');
   throw new Error('Navegador no soporta la Geolocation');
 }
-
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
