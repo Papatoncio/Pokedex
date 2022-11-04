10905ea5c789088a2c9b4b2dbdac7e5c5d9d71e5
AlexRGB <alexismtz200326@gmail.com>
2022-11-04 13:43:01 -0600

    API REST JUNTA

--- server/src/controllers/indexControllers.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class IndexController{
-    public index(req: Request, resp: Response) {
-        resp.send('Hello');
-    }
-}
-
-export const indexController = new IndexController();
